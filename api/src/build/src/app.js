cc86eb5678d73aeddaee4c7c4decfd482e89dbc2
lucas alejandro pardieux <lucaspardieux@gmail.com>
2022-08-02 17:28:02 -0300

    feature (*) : database connection

---
+++ api/src/build/src/app.js
@@ -0,0 +1,26 @@
+"use strict";
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cookieParser());
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+app.use('/', routes);
+app.use((err, _req, res, _next) => {
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+});
+module.exports = app;
