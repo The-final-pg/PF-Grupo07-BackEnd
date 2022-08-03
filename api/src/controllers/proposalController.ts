48fdcb9d3f57036febb1ddbe7f2203409ba10e39
LuisTourn <te.tur.3@gmail.com>
2022-08-03 20:18:23 -0300

    add feature route Proposal with controllers and add includes on offers

--- api/src/controllers/proposalController.ts
+++ api/src/controllers/proposalController.ts
@@ -3,8 +3,8 @@ const { Proposal, Offer } = require("../db");
 
 export const postNewProposal = async (proposal: types.proposal, idOffer: string): Promise<string> => {
     const offer = await Offer.findByPk(idOffer);
-    const newOffer = await Proposal.create(proposal)
-    await offer.addProposal(newOffer);
+    const newProposal = await Proposal.create(proposal);
+    await offer.addProposal(newProposal);
     /*     await newProposal.addOffer(offer); */
     return "Propuesta publicada exitosamente";
 }
