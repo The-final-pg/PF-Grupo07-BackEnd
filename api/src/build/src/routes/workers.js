9b3dc4f687506119bbd1202c590b22068b961da5
juanma2228 <juanmacolaneri@outlook.com>
2022-08-04 17:02:15 -0300

    feat(/routes/workers):add route GET /workers get all workers

--- api/src/build/src/routes/workers.js
+++ api/src/build/src/routes/workers.js
@@ -15,9 +15,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 const workerController_1 = require("../controllers/workerController");
-/* router.get("/worker", async (req:Request, res:Response, next:NextFunction) => {
-
-}) */
+router.get("/", (_req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const worker = yield (0, workerController_1.getAllWorkers)();
+        res.send(worker);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
 router.get("/:idWorker", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const { idWorker } = req.params;
     try {
--- api/src/routes/workers.ts
+++ api/src/routes/workers.ts
@@ -1,26 +1,35 @@
 import express, { NextFunction, Request, Response } from "express";
 const router = express.Router();
-import {WorkerType} from "../types";
-import { getWorkerById} from "../controllers/workerController";
+import { WorkerType } from "../types";
+import { getAllWorkers, getWorkerById } from "../controllers/workerController";
 
-/* router.get("/worker", async (req:Request, res:Response, next:NextFunction) => {
-
-}) */
-
-
-router.get("/:idWorker", async (req:Request, res:Response, next:NextFunction) =>{
-  const {idWorker} = req.params;
+router.get(
+  "/",
+  async (_req: Request, res: Response, next: NextFunction) => {
     try {
-      if(idWorker){
-        const workerById: WorkerType = await getWorkerById(idWorker)
-        return res.json(workerById)
-      } else {
-        throw new Error("worker id not found")
-      }      
+      const worker: WorkerType[] = await getAllWorkers();
+      res.send(worker);
     } catch (error) {
       next(error);
     }
-});
+  }
+);
 
+router.get(
+  "/:idWorker",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { idWorker } = req.params;
+    try {
+      if (idWorker) {
+        const workerById: WorkerType = await getWorkerById(idWorker);
+        return res.json(workerById);
+      } else {
+        throw new Error("worker id not found");
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default router;
