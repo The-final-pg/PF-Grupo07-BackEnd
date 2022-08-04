09bd582ff527598ec120217ae73f0730fadde1cc
juanma2228 <juanmacolaneri@outlook.com>
2022-08-04 19:11:14 -0300

    feat(/controllers_/routes_types.d.ts): add new route search workers all and by name, fix bug in the of one type

--- api/src/build/src/controllers/workerController.js
+++ api/src/build/src/controllers/workerController.js
@@ -9,13 +9,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getWorkerById = exports.getAllWorkers = void 0;
+exports.getWorkerById = exports.getWorkerByName = exports.getAllWorkers = void 0;
+const sequelize_1 = require("sequelize");
 const { UserWorker /* , Review, Proposal, Portfolio */ } = require("../db");
 const getAllWorkers = () => __awaiter(void 0, void 0, void 0, function* () {
     const allWorkers = yield UserWorker.findAll();
     return allWorkers;
 });
 exports.getAllWorkers = getAllWorkers;
+const getWorkerByName = (name) => __awaiter(void 0, void 0, void 0, function* () {
+    const worker = yield UserWorker.findAll({
+        where: {
+            name: {
+                [sequelize_1.Op.iLike]: `%${name}%`
+            },
+        },
+    });
+    return worker;
+});
+exports.getWorkerByName = getWorkerByName;
 const getWorkerById = (id) => __awaiter(void 0, void 0, void 0, function* () {
     const workerById = yield UserWorker.findByPk(parseInt(id) /*,  {
       include: [ Review, Proposal, Portfolio ]
