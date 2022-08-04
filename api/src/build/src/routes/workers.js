b3c34f61dd13a8f8bcd26cf5a859b75d1ec8877a
Juan Manuel Colaneri <102454695+juanma2228@users.noreply.github.com>
2022-08-03 21:39:05 -0300

    Feat/get worker by (#24)
    * first commit
    
    * feat(src/controllers_src/routes_app): add route post/worker/:id add morgan

--- api/src/build/src/routes/workers.js
+++ api/src/build/src/routes/workers.js
@@ -1,1 +1,36 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const router = express_1.default.Router();
+const workerController_1 = require("../controllers/workerController");
+router.get("/:idWorker", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const idWorker = req.params.idWorker;
+    try {
+        if (idWorker) {
+            const workerById = yield (0, workerController_1.getWorkerById)(idWorker);
+            return res.json(workerById);
+        }
+        else {
+            throw new Error("worker id not found");
+        }
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+/* router.post("/", async (req:any, res:any, next:any) =>{
+
+}); */
+exports.default = router;
--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 
+import workerRouter from "./workers";
 import clientRouter from "./clients";
 import offerRouter from './offer';
 import proposalRouter from './proposal'
-//import workerRouter from "./workers";
+
 
 const router = express.Router();
 
 router.use("/clients", clientRouter)
+router.use("/workers", workerRouter)
 router.use("/offer", offerRouter)
 router.use("/proposal", proposalRouter)
-//router.use("/workers", workerRouter)
+
 
 module.exports = router;
