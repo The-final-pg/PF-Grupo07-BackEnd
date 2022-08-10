c76632b141661e82a38af46ce6f41f02d9aa753a
LuisTourn <69329988+LuisTourn@users.noreply.github.com>
2022-08-10 18:49:04 -0300

    Feature route put client (#84)
    * feat(/services_/routes_/controllers): add filters searchOffer and searchWorker by name,rating,profession,remuneration
    
    * feat/combine_filters
    
    * commented the pagination on /offer /worker controllers and services
    
    * new feature route /review with controller
    
    * fix on portfolio post
    
    * new method put on route /client with controller, to modifiy client data
    
    Co-authored-by: juanma2228 <juanmacolaneri@outlook.com>

--- api/src/routes/clients.ts
+++ api/src/routes/clients.ts
@@ -1,7 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 const router = express.Router();
 import { ClientType } from "../types";
-import { getAllClients, getClientById } from "../controllers/clientController";
+import { getAllClients, getClientById, updateClientProfile } from "../controllers/clientController";
 
 router.get("/", async (_req: Request, res: Response, next: NextFunction) => {
   try {
@@ -9,17 +9,29 @@ router.get("/", async (_req: Request, res: Response, next: NextFunction) => {
     res.json(clients);
   } catch (error) {
     next(error);
-  }
+  };
 });
 
-router.get(  "/:id",  async (req: Request, res: Response, next: NextFunction) => {
+router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
     try {
       const client: ClientType = await getClientById(id);
       return res.json(client);
     } catch (error) {
       next(error);
-    }
+    };
   }
 );
+
+router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
+  const id: string = req.params.id;
+  const { name, born_date, photo }: { name: string; born_date: string, photo: string } = req.body;
+  try {
+    const clientUpdate: ClientType = await updateClientProfile(id, name, born_date, photo);
+    res.json(clientUpdate);
+  } catch (error) {
+    next(error);
+  };
+
+});
 export default router;
