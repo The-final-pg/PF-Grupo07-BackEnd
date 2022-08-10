34d4888a5091a3c1887b5ed4580a3487a0da689b
LuisTourn <69329988+LuisTourn@users.noreply.github.com>
2022-08-10 01:08:13 -0300

    Compare problems (#79)
    * feat(/services_/routes_/controllers): add filters searchOffer and searchWorker by name,rating,profession,remuneration
    
    * feat/combine_filters
    
    * commented the pagination on /offer /worker controllers and services
    
    * new feature route /review with controller
    
    * fix on portfolio post
    
    Co-authored-by: juanma2228 <juanmacolaneri@outlook.com>

--- api/src/controllers/portfolioController.ts
+++ api/src/controllers/portfolioController.ts
@@ -1,22 +1,10 @@
 import { PortfolioType } from "../types";
 const { Portfolio, UserWorker } = require("../db");
 
-<<<<<<< HEAD
 export async function postNewPortfolio(portfolio: PortfolioType,
   idWorker: string): Promise<string> {
   const worker = UserWorker.findByPk(idWorker);
   const newPortfolio = await Portfolio.create(portfolio);
   await worker.addPortfolio(newPortfolio);
   return "Portfolio agregado con éxito";
-}
-=======
-export const postNewPortfolio = async (
-  portfolio: PortfolioType,
-  idWorker: string
-): Promise<string> => {
-  const worker = await UserWorker.findByPk(idWorker);
-  const newPortfolio = await Portfolio.create(portfolio);
-  await worker.addPortfolio(newPortfolio);
-  return "Portfolio agregado con éxito";
-};
->>>>>>> 707266edd67172ce2b7f36f2b5f67c9667ca0352
+  }
