af2bbbf37106199dcda4cac384cfe8547d43a7a0
LuisTourn <te.tur.3@gmail.com>
2022-08-10 21:58:12 -0300

    new method put on route /portfolio with controller, to modifiy portfolio data

--- api/src/routes/portfolio.ts
+++ api/src/routes/portfolio.ts
@@ -1,6 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import { PortfolioType } from "../types";
-import { postNewPortfolio } from "../controllers/portfolioController";
+import { postNewPortfolio, updatePortfolio } from "../controllers/portfolioController";
 
 const portfolio = express.Router();
 
@@ -11,6 +11,30 @@ portfolio.post("/:id", async (req: Request, res: Response, next: NextFunction) =
     let response: string;
     response = await postNewPortfolio(portfolio, id);
     res.send(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
+portfolio.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
+  const id: string = req.params.id;
+  const {
+    title,
+    photo,
+    portfolio_description,
+  }: {
+    title: string;
+    photo: string;
+    portfolio_description: string;
+  } = req.body;
+  try {
+    const portfolioUpdate: PortfolioType = await updatePortfolio(
+      id,
+      title,
+      photo,
+      portfolio_description,
+    );
+    res.json(portfolioUpdate);
   } catch (error) {
     next(error);
   }
