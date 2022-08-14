0d902b9dae42b7b12ea00e707f03efd4383163ac
juanma2228 <juanmacolaneri@outlook.com>
2022-08-14 04:08:51 -0300

    change lastname to lastName

--- api/data.ts
+++ api/data.ts
@@ -19,7 +19,7 @@ export const setData = async () => {
     responseUsers.data.map((e) => {
       arrayWorker.push({
         name: e.Worker.name,
-        lastname: e.Worker.lastname,
+        lastName: e.Worker.lastname,
         user_mail: e.Worker.user_mail,
         born_date: e.Worker.born_date,
         password: e.Worker.password,
@@ -32,7 +32,7 @@ export const setData = async () => {
       });
       arrayClient.push({
         name: e.Client.name,
-        lastname: e.Worker.lastname,
+        lastName: e.Worker.lastname,
         user_mail: e.Client.user_mail,
         born_date: e.Client.born_date,
         password: e.Client.password,
