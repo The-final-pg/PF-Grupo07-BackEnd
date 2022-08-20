57b6dba89a890975889887d92b5513e454e9045b
Juan Manuel Colaneri <102454695+juanma2228@users.noreply.github.com>
2022-08-20 19:33:59 -0300

    Update db.js

--- api/src/build/src/db.js
+++ api/src/build/src/db.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const { DATABASE_URL } = process.env;
+const sequelize = new Sequelize(DATABASE_URL, {
     logging: false,
     native: false,
     dialectOptions: {
