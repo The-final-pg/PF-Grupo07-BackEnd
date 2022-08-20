d188c6f12568f7c90b0603fd95d33e664f9c21e6
Juan Manuel Colaneri <102454695+juanma2228@users.noreply.github.com>
2022-08-20 08:02:40 -0300

    Update register.js

--- api/src/build/src/routes/register.js
+++ api/src/build/src/routes/register.js
@@ -128,22 +128,6 @@ register.post("/client", (req, res, next) => __awaiter(void 0, void 0, void 0, f
         else {
             res.send({ message: "Usuario existente. Por favor inicia sesión." });
         }
-        // de toda la info que viene por body, tomamos la password y la hasheamos con el método hash de bcrypt
-        const hashedPassword = yield bcrypt.hash(newClient.password, 8);
-        // el 8 es para el tiempo de las iteraciones, mientras más tiempo más segura, pero con 8 es suficiente.
-        // guardamos en response todo lo que viene de body y la password hasheada,
-        //que la va a recibir la funcion createClient en el controller.
-        let clientCreated;
-        clientCreated = yield (0, registerController_1.createClient)(newClient, hashedPassword);
-        const id = clientCreated.dataValues.id;
-        nodemailerConfig_1.default.sendMail({
-            from: `"REWork" <${REWORK_MAIL}>`,
-            to: clientCreated.dataValues.user_mail,
-            subject: "Bienvenido a REWork",
-            html: `<span>Más de 1000 freelancers disponibles para concretar tus proyectos, ¿qué estás esperando?</span>
-              <b>Confirma tu cuenta <a href="https://rework-xi.vercel.app/confirm/client/${id}"> AQUÍ </a> </b>`
-        });
-        res.send({ message: "Usuario registrado exitosamente! Por favor, verifica tu casilla de correo." });
     }
     catch (error) {
         next(error);
@@ -254,20 +238,6 @@ register.post("/worker", (req, res, next) => __awaiter(void 0, void 0, void 0, f
             res.send({ message: "Usuario existente. Por favor inicia sesión." });
         }
     }catch (error) {
-        const hashedPassword = yield bcrypt.hash(newWorker.password, 8);
-        let workerCreated;
-        workerCreated = yield (0, registerController_1.createWorker)(newWorker, hashedPassword);
-        const id = workerCreated.dataValues.id;
-        nodemailerConfig_1.default.sendMail({
-            from: `"REWork" <${REWORK_MAIL}>`,
-            to: workerCreated.dataValues.user_mail,
-            subject: "Bienvenido a REWork",
-            html: `<span>Más de 1000 proyectos esperando ser concretados, ¿qué esperás para postularte?</span>
-              <b>Confirma tu cuenta <a href="https://rework-xi.vercel.app/confirm/worker/${id}"> AQUÍ </a> </b>`
-        });
-        res.send({ message: "Usuario registrado exitosamente! Por favor, verifica tu casilla de correo." });
-    }
-    catch (error) {
         next(error);
     }
 }));
