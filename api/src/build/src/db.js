95e4c153c6f2929dc59932ee883b361da4417158
juanma2228 <juanmacolaneri@outlook.com>
2022-08-20 19:33:24 -0300

    a verrrr

--- api/src/build/src/db.js
+++ api/src/build/src/db.js
@@ -6,13 +6,7 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
     logging: false,
-    native: false,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        },
-    }
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 const basename = path.basename(__filename);
 const modelDefiners = [];
