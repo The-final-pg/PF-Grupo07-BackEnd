613fdae63126cfa88a6b4ce27f9d2e4c7a2ee56e
juanma2228 <juanmacolaneri@outlook.com>
2022-08-20 10:52:57 -0300

    feat:added new skills

--- api/index.ts
+++ api/index.ts
@@ -51,14 +51,14 @@ import {
   setReview,
 } from "./data";
 import { cleanDataBase } from './src/services/cleanDataBase'
-
+const { PORT } = process.env
 
 
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(3001, async () => {
-    console.log(`%s listening at 3001`); // eslint-disable-line no-console
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, async () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
     await setData();
     await setOffers();
     await setProposals();
