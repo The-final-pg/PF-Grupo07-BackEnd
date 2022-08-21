683587b29b5cc957ae20e711bd68cc5cec827230
lucas alejandro pardieux <lucaspardieux@gmail.com>
2022-08-21 20:40:57 -0300

    bugfixes

--- api/src/build/src/db.js
+++ api/src/build/src/db.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-    logging: false, 
+    logging: false,
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 const basename = path.basename(__filename);
