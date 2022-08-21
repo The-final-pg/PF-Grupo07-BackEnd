796a78a49ddac48189bdaf58dee40301a8d795cf
Lucia Chinni <97314099+luchinni@users.noreply.github.com>
2022-08-21 01:22:32 -0300

    Firebase (#141)
    * google config sin terminar
    
    * cambios en app (cors), template mail html y login con google sin funcionar
    
    * ANDA EL LOGIN DE GOOGLE WORKER
    
    * firebase google login
    
    * firebase
    
    * cambio de config
    
    * creacion client google
    
    * login google client conectado
    
    * login y registro con google firebase funcionando
    
    * login y registro google firebase en testing
    
    * mail google client funcionando excelentisimamente
    
    Co-authored-by: Franco Fraiese <fraancofraiese@gmail.com>
    Co-authored-by: Franco Fraiese <86889319+francofraiese@users.noreply.github.com>

--- api/src/build/src/routes/login.js
+++ api/src/build/src/routes/login.js
@@ -50,8 +50,84 @@ login.post("/", (req, res, next) => __awaiter(void 0, void 0, void 0, function* 
                 isAdmin: user.isAdmin,
                 isWorker: user.isWorker,
                 premium: user.premium
-            }, SECRET_KEY, { expiresIn: "10m" }));
+            }, SECRET_KEY, { expiresIn: "8h" }));
         }
     }))(req, res, next);
 }));
 exports.default = login;
+/* router.post("/email", async (req, res, next) => {
+    var { email, type } = req.body;
+    email = email.toLowerCase();
+    const user = await User.findOne({ where: { email } });
+    if (!user) return res.json({ error: "Non-existent User" });
+    let token = await jwt.sign({ email }, SECRET_KEY, { expiresIn: "1000hr" });
+    if (type === "passwordreset") {
+        transporter.sendMail({
+            from: `"On The Rocks" <${GMAIL_APP_EMAIL}>`, // sender address
+            to: email, // list of receivers
+            subject: "Recover password", // Subject line
+            text: "click on the link to reset your password ", // plain text body
+            html: `<b>click on the link to reset your password: <a href="${FRONT}/verify/password?token=${token}"> HERE </a> </b>`, // html body
+        });
+        res.json({ success: "Email sent" });
+    }
+    if (type === "verifyadmin") {
+        let token = await jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                isDeleted: user.isDeleted,
+                Authenticated: true,
+            },
+            SECRET_KEY,
+        //    { expiresIn: 60 * 10 } // 10 min
+           { expiresIn: 60 * 60 } // 1hr
+        );
+        await transporter.sendMail({
+            from: `"On The Rocks" <${GMAIL_APP_EMAIL}>`, // sender address
+            to: email, // list of receivers
+            subject: "Two steps verification", // Subject line
+            text: "Click on the link to verify your identity: ", // plain text body
+            html: `<b>Click on the link to verify your identity: <a href="${FRONT}/verify/admin?token=${token}"> HERE </a> </b>`, // html body
+        });
+        res.json({ success: "Email sent" });
+    }
+});
+
+router.post(
+    "/passwordreset",
+    passport.authenticate("bearer", { session: false }),
+    async (req, res, next) => {
+        let { token, newPassword } = req.body;
+        try {
+            let email = jwt.verify(token, SECRET_KEY).email.toLowerCase();
+            let isRegistered = await User.findOne({ where: { email: email } });
+            if (isRegistered) {
+                const hashedPassword = await bcrypt.hash(newPassword, 12);
+                await User.update(
+                    { password: hashedPassword },
+                    { where: { email: email } }
+                );
+                return res.json({ success: "Updated user" });
+            } else {
+                return res.json({ error: "Non-existent User" });
+            }
+        } catch (e) {
+            return res.json({ error: e.message });
+        }
+    }
+);
+
+router.post("/admin", async (req, res, next) => {
+    let token = req.body;
+    try {
+        let email = jwt.verify(token, SECRET_KEY).email.toLowerCase();
+        let isAdmin = await User.findOne({
+            where: { email: email, isAdmin: true },
+        });
+        isAdmin ? res.json(true) : res.json(false);
+    } catch (e) {
+        return res.json({ error: e.message });
+    }
+}); */
--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,8 +1,10 @@
 import express, { NextFunction, Request, Response } from "express";
 const auth = express.Router();
+const bcrypt = require("bcrypt");
 import jwt from "jsonwebtoken";
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY, REWORK_MAIL } = process.env;
 const { UserWorker, UserClient } = require("../db");
+import transporter from "../utils/nodemailer/nodemailerConfig";
 
 auth.post("/", async(req: Request, res: Response, next: NextFunction) => {
     const googleUser = req.body
@@ -10,7 +12,7 @@ auth.post("/", async(req: Request, res: Response, next: NextFunction) => {
         console.log(googleUser)
         const clientFound = await UserClient.findOne({where: {user_mail: googleUser?.user_mail}})
         const workerFound = await UserWorker.findOne({where: {user_mail: googleUser?.user_mail}})
-        console.log("clien", clientFound)
+        console.log("client", clientFound)
         console.log("worker", workerFound)
         if(clientFound){
             return res.send(jwt.sign(
@@ -39,7 +41,6 @@ auth.post("/", async(req: Request, res: Response, next: NextFunction) => {
                 ))
             /* res.status(200).json(workerFound) */
         } else {
-            // llevar a la ruta que lo hace elegir entre worker y client y despues crearlo
             res.send ('usuario no encontrado' )
         }
 
@@ -49,9 +50,167 @@ auth.post("/", async(req: Request, res: Response, next: NextFunction) => {
     }
 })
 
-auth.post("/client")
+auth.post("/client", async(req: Request, res: Response, next: NextFunction) => {
+    const newClient = req.body;
+    try {
+        console.log("newClient", newClient)
+        const hashedPassword = await bcrypt.hash(newClient.password, 8)
+        const clientGoogle = await UserClient.create({
+            name: newClient.name,
+            lastName: newClient.lastName,
+            user_mail: newClient.user_mail,
+            born_date: newClient.born_date,
+            password: hashedPassword,         
+            rating: newClient.rating,
+            notification: newClient.notification,
+            photo: newClient.photo,
+            isActive: true,
+            isWorker: false,
+            isAdmin: false,
+            premium: false
+        })
 
-auth.post("/worker")
+        transporter.sendMail({
+          from: `"REWork" <${REWORK_MAIL}>`,
+          to: clientGoogle.user_mail,
+          subject: "¡Te damos la bienvenida a REwork!",
+          html: `<!DOCTYPE html>
+          <html lang="en">
+          <head>
+              <meta charset="UTF-8">
+              <meta http-equiv="X-UA-Compatible" content="IE=edge">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          
+              <style>
+                  p, a, h1, h2, h3, h4, h5, h6 {font-family: 'Poppins', sans-serif !important;}
+                  h1{ font-size: 30px !important;}
+                  h2{ font-size: 25px !important;}
+                  h3{ font-size: 18px !important;}
+                  h4{ font-size: 16px !important;}
+                  p{font-size: 15px !important;}
+                  a{font-size: 30px !important;}
+          
+                  .claseBoton{
+                      width: 30%;
+                          background-color: #fcae3b;
+                          border: 2px solid #fcae3b;
+                          color: black; 
+                          padding: 16px 32px;
+                          text-align: center;
+                          text-decoration: none;
+                          font-weight: bold;
+                          display: inline-block;
+                          font-size: 16px;
+                          margin: 4px 2px;
+                          transition-duration: 0.4s;
+                          cursor: pointer;
+                  }
+                  .claseBoton:hover{
+                      background-color: #000000;
+                      color: #ffffff;
+                  }
+                  .imag{
+                      width: 20px;
+                      height: 20px;
+                  }
+                  .contA{
+                      margin: 0px 5px 0 5px;
+                  }
+                  .afooter{
+                      color: #ffffff !important; 
+                      text-decoration: none;
+                      font-size: 13px !important;
+                  }
+              </style>
+          </head>
+          <body>
+              <div style="width: 100%; background-color: #e3e3e3;">
+                  <div style="padding: 20px 10px 20px 10px;">
+                      <!-- Contenido principal -->
+                      <div style="background-color: #ffffff; padding: 20px 0px 5px 0px; width: 100%; text-align: center;">
+                          <h1>¡Hola ${clientGoogle.name}!</h1>
+                          <h3>Tu cuenta en REwork fue creada con éxito.</h3>
+                          <p>Tu contraseña asignada momentáneamente es: ${newClient.password}</p>
+          
+                          <!-- Gracias -->
+                          <p>Te sugerimos cambiar tu contraseña por una que solo tú conozcas. Puedes hacerlo desde</p>
+          
+                          <!-- Botón -->
+                          <a class="claseBoton" href="http://localhost:3000/myProfile">AQUÍ</a>
+                      </div>
+                      <!-- Contenido principal -->
+          
+                      <!-- Footer -->
+                      <div style="background-color: #ffffff; color: #ffffff; padding: 5px 0px 0px 0px; width: 100%; text-align: center;">
+                          <p style="background-color: black; padding: 10px 0px 10px 0px; font-size: 12px !important;">
+                              © 2022 REwork, todos los derechos reservados.
+                          </p>
+                      </div>
+                      <!-- Footer -->
+          
+          
+          
+                  </div>
+              </div>
+          </body>
+          </html>`
+      })
+        
+
+        res.send(jwt.sign(
+            {
+                id: clientGoogle.id,
+                user_mail: clientGoogle.user_mail,
+                isAdmin: clientGoogle.isAdmin,
+                isWorker: clientGoogle.isWorker,
+                premium: clientGoogle.premium
+            },
+            SECRET_KEY,
+            { expiresIn: "8h" }
+            ))
+    } catch(error){
+        next(error)
+    }
+})
+
+auth.post("/worker", async(req: Request, res: Response, next: NextFunction) => {
+    const newWorker = req.body;
+    try {
+        console.log("newWorker", newWorker)
+        const hashedPassword = await bcrypt.hash(newWorker.uid, 8)
+        
+        const workerGoogle = await UserWorker.create({
+            name: newWorker.name,
+            password: hashedPassword,
+            lastName: newWorker.lastName,
+            user_mail: newWorker.user_mail,
+            born_date: newWorker.born_date,
+            rating: newWorker.rating,
+            notification: newWorker.notification,
+            photo: newWorker.photo,
+            isActive: true,
+            isWorker: true,
+            premium: false,
+            isAdmin: true
+        })
+
+        res.send(workerGoogle)
+
+        return res.send(jwt.sign(
+            {
+                id: workerGoogle.id,
+                user_mail: workerGoogle.user_mail,
+                isAdmin: workerGoogle.isAdmin,
+                isWorker: workerGoogle.isWorker,
+                premium: workerGoogle.premium
+            },
+            SECRET_KEY,
+            { expiresIn: "8h" }
+            ))
+    } catch(error){
+        next(error)
+    }
+})
 
 
 
