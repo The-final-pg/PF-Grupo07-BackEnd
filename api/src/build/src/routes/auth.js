4e1578985ae27726abb55df9fe000d2a8a4c191a
Lucia Chinni <luchinni8@gmail.com>
2022-08-21 01:19:30 -0300

    mail google client funcionando excelentisimamente

--- api/src/build/src/routes/auth.js
+++ api/src/build/src/routes/auth.js
@@ -16,17 +16,16 @@ const express_1 = __importDefault(require("express"));
 const auth = express_1.default.Router();
 const bcrypt = require("bcrypt");
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-const { SECRET_KEY } = process.env;
-const { UserWorker, UserClient } = require("../db"); /*
-import { createGoogleClient, createGoogleWorker } from "../controllers/authController";
-import { ClientType } from "../types"; */
+const { SECRET_KEY, REWORK_MAIL } = process.env;
+const { UserWorker, UserClient } = require("../db");
+const nodemailerConfig_1 = __importDefault(require("../utils/nodemailer/nodemailerConfig"));
 auth.post("/", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const googleUser = req.body;
     try {
         console.log(googleUser);
         const clientFound = yield UserClient.findOne({ where: { user_mail: googleUser === null || googleUser === void 0 ? void 0 : googleUser.user_mail } });
         const workerFound = yield UserWorker.findOne({ where: { user_mail: googleUser === null || googleUser === void 0 ? void 0 : googleUser.user_mail } });
-        console.log("clien", clientFound);
+        console.log("client", clientFound);
         console.log("worker", workerFound);
         if (clientFound) {
             return res.send(jsonwebtoken_1.default.sign({
@@ -74,6 +73,91 @@ auth.post("/client", (req, res, next) => __awaiter(void 0, void 0, void 0, funct
             isWorker: false,
             isAdmin: false,
             premium: false
+        });
+        nodemailerConfig_1.default.sendMail({
+            from: `"REWork" <${REWORK_MAIL}>`,
+            to: clientGoogle.user_mail,
+            subject: "¡Te damos la bienvenida a REwork!",
+            html: `<!DOCTYPE html>
+          <html lang="en">
+          <head>
+              <meta charset="UTF-8">
+              <meta http-equiv="X-UA-Compatible" content="IE=edge">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          
+              <style>
+                  p, a, h1, h2, h3, h4, h5, h6 {font-family: 'Poppins', sans-serif !important;}
+                  h1{ font-size: 30px !important;}
+                  h2{ font-size: 25px !important;}
+                  h3{ font-size: 18px !important;}
+                  h4{ font-size: 16px !important;}
+                  p{font-size: 15px !important;}
+                  a{font-size: 30px !important;}
+          
+                  .claseBoton{
+                      width: 30%;
+                          background-color: #fcae3b;
+                          border: 2px solid #fcae3b;
+                          color: black; 
+                          padding: 16px 32px;
+                          text-align: center;
+                          text-decoration: none;
+                          font-weight: bold;
+                          display: inline-block;
+                          font-size: 16px;
+                          margin: 4px 2px;
+                          transition-duration: 0.4s;
+                          cursor: pointer;
+                  }
+                  .claseBoton:hover{
+                      background-color: #000000;
+                      color: #ffffff;
+                  }
+                  .imag{
+                      width: 20px;
+                      height: 20px;
+                  }
+                  .contA{
+                      margin: 0px 5px 0 5px;
+                  }
+                  .afooter{
+                      color: #ffffff !important; 
+                      text-decoration: none;
+                      font-size: 13px !important;
+                  }
+              </style>
+          </head>
+          <body>
+              <div style="width: 100%; background-color: #e3e3e3;">
+                  <div style="padding: 20px 10px 20px 10px;">
+                      <!-- Contenido principal -->
+                      <div style="background-color: #ffffff; padding: 20px 0px 5px 0px; width: 100%; text-align: center;">
+                          <h1>¡Hola ${clientGoogle.name}!</h1>
+                          <h3>Tu cuenta en REwork fue creada con éxito.</h3>
+                          <p>Tu contraseña asignada momentáneamente es: ${newClient.password}</p>
+          
+                          <!-- Gracias -->
+                          <p>Te sugerimos cambiar tu contraseña por una que solo tú conozcas. Puedes hacerlo desde</p>
+          
+                          <!-- Botón -->
+                          <a class="claseBoton" href="http://localhost:3000/myProfile">AQUÍ</a>
+                      </div>
+                      <!-- Contenido principal -->
+          
+                      <!-- Footer -->
+                      <div style="background-color: #ffffff; color: #ffffff; padding: 5px 0px 0px 0px; width: 100%; text-align: center;">
+                          <p style="background-color: black; padding: 10px 0px 10px 0px; font-size: 12px !important;">
+                              © 2022 REwork, todos los derechos reservados.
+                          </p>
+                      </div>
+                      <!-- Footer -->
+          
+          
+          
+                  </div>
+              </div>
+          </body>
+          </html>`
         });
         res.send(jsonwebtoken_1.default.sign({
             id: clientGoogle.id,
--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -2,10 +2,9 @@ import express, { NextFunction, Request, Response } from "express";
 const auth = express.Router();
 const bcrypt = require("bcrypt");
 import jwt from "jsonwebtoken";
-const { SECRET_KEY } = process.env;
-const { UserWorker, UserClient } = require("../db");/* 
-import { createGoogleClient, createGoogleWorker } from "../controllers/authController";
-import { ClientType } from "../types"; */
+const { SECRET_KEY, REWORK_MAIL } = process.env;
+const { UserWorker, UserClient } = require("../db");
+import transporter from "../utils/nodemailer/nodemailerConfig";
 
 auth.post("/", async(req: Request, res: Response, next: NextFunction) => {
     const googleUser = req.body
@@ -13,7 +12,7 @@ auth.post("/", async(req: Request, res: Response, next: NextFunction) => {
         console.log(googleUser)
         const clientFound = await UserClient.findOne({where: {user_mail: googleUser?.user_mail}})
         const workerFound = await UserWorker.findOne({where: {user_mail: googleUser?.user_mail}})
-        console.log("clien", clientFound)
+        console.log("client", clientFound)
         console.log("worker", workerFound)
         if(clientFound){
             return res.send(jwt.sign(
@@ -70,6 +69,93 @@ auth.post("/client", async(req: Request, res: Response, next: NextFunction) => {
             isAdmin: false,
             premium: false
         })
+
+        transporter.sendMail({
+          from: `"REWork" <${REWORK_MAIL}>`,
+          to: clientGoogle.user_mail,
+          subject: "¡Te damos la bienvenida a REwork!",
+          html: `<!DOCTYPE html>
+          <html lang="en">
+          <head>
+              <meta charset="UTF-8">
+              <meta http-equiv="X-UA-Compatible" content="IE=edge">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          
+              <style>
+                  p, a, h1, h2, h3, h4, h5, h6 {font-family: 'Poppins', sans-serif !important;}
+                  h1{ font-size: 30px !important;}
+                  h2{ font-size: 25px !important;}
+                  h3{ font-size: 18px !important;}
+                  h4{ font-size: 16px !important;}
+                  p{font-size: 15px !important;}
+                  a{font-size: 30px !important;}
+          
+                  .claseBoton{
+                      width: 30%;
+                          background-color: #fcae3b;
+                          border: 2px solid #fcae3b;
+                          color: black; 
+                          padding: 16px 32px;
+                          text-align: center;
+                          text-decoration: none;
+                          font-weight: bold;
+                          display: inline-block;
+                          font-size: 16px;
+                          margin: 4px 2px;
+                          transition-duration: 0.4s;
+                          cursor: pointer;
+                  }
+                  .claseBoton:hover{
+                      background-color: #000000;
+                      color: #ffffff;
+                  }
+                  .imag{
+                      width: 20px;
+                      height: 20px;
+                  }
+                  .contA{
+                      margin: 0px 5px 0 5px;
+                  }
+                  .afooter{
+                      color: #ffffff !important; 
+                      text-decoration: none;
+                      font-size: 13px !important;
+                  }
+              </style>
+          </head>
+          <body>
+              <div style="width: 100%; background-color: #e3e3e3;">
+                  <div style="padding: 20px 10px 20px 10px;">
+                      <!-- Contenido principal -->
+                      <div style="background-color: #ffffff; padding: 20px 0px 5px 0px; width: 100%; text-align: center;">
+                          <h1>¡Hola ${clientGoogle.name}!</h1>
+                          <h3>Tu cuenta en REwork fue creada con éxito.</h3>
+                          <p>Tu contraseña asignada momentáneamente es: ${newClient.password}</p>
+          
+                          <!-- Gracias -->
+                          <p>Te sugerimos cambiar tu contraseña por una que solo tú conozcas. Puedes hacerlo desde</p>
+          
+                          <!-- Botón -->
+                          <a class="claseBoton" href="http://localhost:3000/myProfile">AQUÍ</a>
+                      </div>
+                      <!-- Contenido principal -->
+          
+                      <!-- Footer -->
+                      <div style="background-color: #ffffff; color: #ffffff; padding: 5px 0px 0px 0px; width: 100%; text-align: center;">
+                          <p style="background-color: black; padding: 10px 0px 10px 0px; font-size: 12px !important;">
+                              © 2022 REwork, todos los derechos reservados.
+                          </p>
+                      </div>
+                      <!-- Footer -->
+          
+          
+          
+                  </div>
+              </div>
+          </body>
+          </html>`
+      })
+        
 
         res.send(jwt.sign(
             {
