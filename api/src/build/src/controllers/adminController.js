44634f34aeb0da24cb419a070bc8f392a5fea675
LuisTourn <te.tur.3@gmail.com>
2022-08-22 00:37:51 -0300

    add new routes on admin route, methos put to add profession or skills

--- api/src/build/src/controllers/adminController.js
+++ api/src/build/src/controllers/adminController.js
@@ -9,8 +9,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllUsers = void 0;
+exports.addNewSkills = exports.addNewProfessions = exports.getAllUsers = void 0;
 const { UserClient, UserWorker } = require("../db");
+const id = "3748eb17-a207-5bc3-aa4f-3113a1b9409d";
 function getAllUsers() {
     return __awaiter(this, void 0, void 0, function* () {
         let allClients = yield UserClient.findAll();
@@ -20,3 +21,35 @@ function getAllUsers() {
     });
 }
 exports.getAllUsers = getAllUsers;
+function addNewProfessions(professions) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const workerData = yield UserWorker.findByPk(id, { attributes: ["profession"] });
+        const totalProfessions = workerData.toJSON();
+        const totalNewProfessions = totalProfessions.profession;
+        professions.forEach((e) => totalNewProfessions.includes(e) ? null : totalNewProfessions.push(e));
+        yield UserWorker.update({
+            profession: totalNewProfessions,
+        }, { where: {
+                id,
+            }
+        });
+        return totalNewProfessions;
+    });
+}
+exports.addNewProfessions = addNewProfessions;
+function addNewSkills(skills) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const workerData = yield UserWorker.findByPk(id, { attributes: ["skills"] });
+        const totalSkills = workerData.toJSON();
+        const totalNewSkills = totalSkills.skills;
+        skills.forEach((e) => totalNewSkills.includes(e) ? null : totalNewSkills.push(e));
+        yield UserWorker.update({
+            skills: totalNewSkills,
+        }, { where: {
+                id,
+            }
+        });
+        return totalNewSkills;
+    });
+}
+exports.addNewSkills = addNewSkills;
--- api/src/routes/admin.ts
+++ api/src/routes/admin.ts
@@ -1,12 +1,32 @@
 import express, { NextFunction, Request, Response } from "express";
 const admin = express.Router();
 import { ClientType, WorkerType } from "../types";
-import { getAllUsers } from "../controllers/adminController";
+import { getAllUsers, addNewProfessions, addNewSkills } from "../controllers/adminController";
 
 admin.get("/users", async (_req: Request, res: Response, next: NextFunction) => {
     try {
       const users: Array<ClientType | WorkerType> = await getAllUsers();
       res.json(users);
+    } catch (error) {
+      next(error);
+    };
+  });
+
+  admin.put("/profession", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data: string[] = req.body.profession;
+      const profession: string[] = await addNewProfessions(data);
+      res.json(profession);
+    } catch (error) {
+      next(error);
+    };
+  });
+
+  admin.put("/skills", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data: string[] = req.body.skills;
+      const skills: string[] = await addNewSkills(data);
+      res.json(skills);
     } catch (error) {
       next(error);
     };
