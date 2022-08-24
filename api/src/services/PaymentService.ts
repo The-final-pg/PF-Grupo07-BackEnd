1231d6c902d7ffef97559fb7e7142b0f969bb6b0
Lucas Pardieux <98245636+LucasPardieux@users.noreply.github.com>
2022-08-24 07:04:16 -0300

    fix back (#216)

--- api/src/services/PaymentService.ts
+++ api/src/services/PaymentService.ts
@@ -24,9 +24,9 @@ class PaymentService {
         }
       ],
       back_urls: {
-        failure: "http://localhost:3000/failure",
-        pending: "http://localhost:3000/pending",
-        success: `http://localhost:3000/success/${currentOffer.idOffer}`
+        failure: "https://rework-xi.vercel.app/failure",
+        pending: "https://rework-xi.vercel.app/pending",
+        success: `https://rework-xi.vercel.app/success/${currentOffer.idOffer}`
       }
     };
 
