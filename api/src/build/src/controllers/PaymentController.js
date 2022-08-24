f25e49dfda09f32f277dacdec326518c5dc7ebd4
juanma2228 <juanmacolaneri@outlook.com>
2022-08-24 17:11:32 -0300

    Que se yo

--- api/src/build/src/controllers/PaymentController.js
+++ api/src/build/src/controllers/PaymentController.js
@@ -83,6 +83,7 @@ class PaymentController {
         return __awaiter(this, void 0, void 0, function* () {
             try {
                 this.subscriptionService.getMPInfo(req.body);
+                localStorage.removeItem("token");
             }
             catch (error) {
                 next(error);
