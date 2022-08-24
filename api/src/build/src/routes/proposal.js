b2c20990d4b62daf3daa6fa7da4e069643b47dad
juanma2228 <juanmacolaneri@outlook.com>
2022-08-24 20:39:10 -0300

    mergeDev

--- api/src/build/src/routes/proposal.js
+++ api/src/build/src/routes/proposal.js
@@ -917,14 +917,4 @@ proposal.put("/:id", (req, res, next) => __awaiter(void 0, void 0, void 0, funct
     }
     ;
 }));
-proposal.get("/:id", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    const { id } = req.params;
-    try {
-        const proposal = yield (0, proposalController_1.getProposalById)(id);
-        return res.json(proposal);
-    }
-    catch (error) {
-        next(error);
-    }
-}));
 exports.default = proposal;
